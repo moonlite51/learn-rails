eb08b3ee3e1c68620298d6abd4a614970c59b853
Lusiana Pontan <letters3704@gmail.com>
2017-12-21 16:16:30 +1100

    fixed mistakes in development.rb to run rails server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,15 +45,15 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: Rails.application.secrets.domain.name,
-    authentication: "plain"
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
   # Can use port 25, 587, 2525
   # ActionMailer config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
